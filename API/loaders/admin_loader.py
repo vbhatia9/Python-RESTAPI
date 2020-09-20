b53ae66076a7386610472cc187cf7e065a1029fa
Vijay Bhatia <vbhatia991@gmail.com>
2020-09-20 02:26:20 -0400

    New code commits

--- API/loaders/admin_loader.py
+++ API/loaders/admin_loader.py
@@ -1,3 +1,13 @@
 import os
-import path
 from common_utils import logger
+import requests
+from pymongo import MongoClient
+
+mongo=os.environ['MONGO']
+#print(f"Mongo {mongo}")
+client = MongoClient(mongo)
+# Set the db object to point to the business database
+db=client.sample_airbnb
+
+pricerangecount = db.listingsAndReviews.find({'price':80}).count()
+print(pricerange)
--- API/scripts/setEnv.sh
+++ API/scripts/setEnv.sh
@@ -1,3 +1,4 @@
-export PYTHONPATH=$"(\pwd)"
-export MONGO=mongodb+srv://user_admin_vijay:*****@cluster0.dbinn.mongodb.net/test?authSource=admin&replicaSet=atlas-lh70qc-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
-export URL=
+export PYTHONPATH="$(\pwd)"
+#export URL=
+export MONGO="mongodb+srv://user_admin_vijay:******@cluster0.dbinn.mongodb.net/<dbname>?retryWrites=true&w=majority"
+#export MONGO=mongodb+srv://user_admin_vijay:*****@cluster0.dbinn.mongodb.net/test?authSource=admin&replicaSet=atlas-lh70qc-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
